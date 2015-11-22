626d37976f3fa5c51edc7e94936309d76c56f610
Anthony Giuliano <agiuliano@gmail.com>
2015-11-21 20:32:26 -0500

    Completes exercises for chapter 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,6 +7,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 			post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
 		end
 		assert_template 'users/new'
+		assert_template partial: '_error_messages'
 	end
 
 	test "valid signup information" do
@@ -15,5 +16,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 			post_via_redirect users_path, user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
 		end
 		assert_template 'users/show'
+		assert_not flash.empty?
 	end
 end
